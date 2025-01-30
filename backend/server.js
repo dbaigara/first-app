163e2bcde870271b9633e78776f4a673b4508bc9
Dosbol Baigarashev <dbaigara@cern.ch>
2025-01-30 14:10:34 +0300

    Added GET /tasks endpoint

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const db = require('./db');
+
+app.get('/tasks', (req, res) => {
+  db.all('SELECT * FROM tasks', (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
