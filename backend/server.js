45ccc0f6d7f1173d33e59c17d6e734f4b7c35263
Dosbol Baigarashev <dbaigara@cern.ch>
2025-01-31 16:51:21 +0300

    change DATABASE_URL to PORT in server side

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const port = process.env.DATABASE_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
