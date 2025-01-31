3409472b9b0cf0ba91dfc17f5ef89db163fcb543
Dosbol Baigarashev <dbaigara@cern.ch>
2025-01-31 16:24:05 +0300

    Add Render link to connect front with back

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'https://first-app-ldvx.onrender.com',
       changeOrigin: true,
     })
   );
